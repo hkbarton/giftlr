5bdf885be48cd88f5906c7d2ec6638803edd48ad
kehuang <kehuang@box.com>
2015-03-18 11:13:14 -0700

    payment setting update

--- giftlr/PSCreationTableViewCell.m
+++ giftlr/PSCreationTableViewCell.m
@@ -7,8 +7,22 @@
 //
 
 #import "PSCreationTableViewCell.h"
+#import "PTKView.h"
+
+@interface PSCreationTableViewCell() <PTKViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIButton *btnCreation;
+
+- (IBAction)onBtnCreationClicked:(id)sender;
+
+@property (nonatomic, strong) PTKView *cardInputView;
+
+@end
 
 @implementation PSCreationTableViewCell
+
+NSInteger const CreationType_CreaditCard = 0;
+NSInteger const CreationType_Bank = 1;
 
 - (void)awakeFromNib {
     self.selectionStyle = UITableViewCellSelectionStyleNone;
@@ -16,6 +30,43 @@
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
+}
+
+- (void)setCreationType:(NSInteger)creationType {
+    _creationType = creationType;
+    if (self.creationType == CreationType_CreaditCard) {
+        [self.btnCreation setImage:[UIImage imageNamed:@"add-card"] forState:UIControlStateNormal];
+    } else {
+        [self.btnCreation setImage:[UIImage imageNamed:@"add-bank"] forState:UIControlStateNormal];
+    }
+}
+
+- (IBAction)onBtnCreationClicked:(id)sender {
+    if (self.creationType == CreationType_CreaditCard) {
+        if (!self.cardInputView) {
+            self.cardInputView = [[PTKView alloc] initWithFrame:CGRectMake(0, 0, self.bounds.size.width - 20, self.bounds.size.height)];
+            // hack here, remove original background
+            [self.cardInputView.subviews[0] removeFromSuperview];
+            self.cardInputView.delegate = self;
+        }
+        [self addSubview:self.cardInputView];
+        self.cardInputView.alpha = 0;
+        [UIView animateWithDuration:0.3 animations:^{
+            CGPoint center = self.btnCreation.center;
+            center.x += self.frame.size.width/2;
+            self.btnCreation.alpha = 0;
+            self.btnCreation.center = center;
+            self.cardInputView.alpha = 1;
+        } completion:nil];
+    } else {
+        
+    }
+}
+
+- (void)paymentView:(PTKView *)view withCard:(PTKCard *)card isValid:(BOOL)valid {
+    if (valid) {
+        // TODO
+    }
 }
 
 @end
