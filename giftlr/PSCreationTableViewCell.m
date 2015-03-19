a6b02a01e4566f88d0b8e8dce4ccb40e3d2ea0d8
kehuang <kehuang@box.com>
2015-03-18 18:44:52 -0700

    update payment setting view

--- giftlr/PSCreationTableViewCell.m
+++ giftlr/PSCreationTableViewCell.m
@@ -12,10 +12,16 @@
 @interface PSCreationTableViewCell() <PTKViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIButton *btnCreation;
+@property (weak, nonatomic) IBOutlet UIButton *btnOK;
+@property (weak, nonatomic) IBOutlet UIButton *btnCancel;
 
 - (IBAction)onBtnCreationClicked:(id)sender;
+- (IBAction)btnOKClicked:(id)sender;
+- (IBAction)btnCancelClicked:(id)sender;
 
 @property (nonatomic, strong) PTKView *cardInputView;
+
+@property (nonatomic, assign) CGPoint oriCenterOfCreationButton;
 
 @end
 
@@ -26,6 +32,12 @@ NSInteger const CreationType_Bank = 1;
 
 - (void)awakeFromNib {
     self.selectionStyle = UITableViewCellSelectionStyleNone;
+    [self.btnOK setImage:[UIImage imageNamed:@"ps-ok"] forState:UIControlStateNormal];
+    [self.btnOK setImage:[UIImage imageNamed:@"ps-ok-disable"] forState:UIControlStateDisabled];
+    [self.btnCancel setImage:[UIImage imageNamed:@"ps-cancel"] forState:UIControlStateNormal];
+    self.btnOK.enabled = NO;
+    self.btnOK.hidden = YES;
+    self.btnCancel.hidden = YES;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
@@ -41,6 +53,26 @@ NSInteger const CreationType_Bank = 1;
     }
 }
 
+- (void)hideInputView {
+    if (self.creationType == CreationType_CreaditCard) {
+        [UIView animateWithDuration:0.2 animations:^{
+            self.cardInputView.alpha = 0;
+            self.btnOK.alpha = 0;
+            self.btnCancel.alpha = 0;
+        } completion:^(BOOL finished) {
+            [self.cardInputView removeFromSuperview];
+            self.btnOK.hidden = YES;
+            self.btnCancel.hidden = YES;
+            [UIView animateWithDuration:0.2 animations:^{
+                self.btnCreation.center = self.oriCenterOfCreationButton;
+                self.btnCreation.alpha = 1;
+            }];
+        }];
+    } else {
+        
+    }
+}
+
 - (IBAction)onBtnCreationClicked:(id)sender {
     if (self.creationType == CreationType_CreaditCard) {
         if (!self.cardInputView) {
@@ -52,21 +84,38 @@ NSInteger const CreationType_Bank = 1;
         [self addSubview:self.cardInputView];
         self.cardInputView.alpha = 0;
         [UIView animateWithDuration:0.3 animations:^{
+            self.oriCenterOfCreationButton = self.btnCreation.center;
             CGPoint center = self.btnCreation.center;
             center.x += self.frame.size.width/2;
             self.btnCreation.alpha = 0;
             self.btnCreation.center = center;
             self.cardInputView.alpha = 1;
-        } completion:nil];
+        } completion:^(BOOL finished) {
+            self.btnOK.hidden = NO;
+            self.btnCancel.hidden = NO;
+            self.btnOK.alpha = 0;
+            self.btnCancel.alpha = 0;
+            [UIView animateWithDuration:0.3 animations:^{
+                self.btnOK.alpha = 1;
+                self.btnCancel.alpha = 1;
+            }];
+        }];
     } else {
         
     }
 }
 
+- (IBAction)btnOKClicked:(id)sender {
+    [self hideInputView];
+}
+
+- (IBAction)btnCancelClicked:(id)sender {
+    [self hideInputView];
+    //self.cardInputView.cardNumber = nil;
+}
+
 - (void)paymentView:(PTKView *)view withCard:(PTKCard *)card isValid:(BOOL)valid {
-    if (valid) {
-        // TODO
-    }
+    self.btnOK.enabled = valid;
 }
 
 @end
